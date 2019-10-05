5f26f96656058ae22be1c890dff5cfad4840f79c
Magdalena Kotołowska <m.kotolowska@gmail.com>
2019-10-05 12:18:43 +0200

    Add foreign key to vote migration

--- db/migrate/20191005095836_create_votes.rb
+++ db/migrate/20191005095836_create_votes.rb
@@ -1,6 +1,7 @@
 class CreateVotes < ActiveRecord::Migration[5.2]
   def change
     create_table :votes do |t|
+      t.references :voting, foreign_key: true
       t.string :name
       t.decimal :number
 
