77c57bd8df3cd5f688de1862b6bcef9256d9359e
Magdalena Kotołowska <m.kotolowska@gmail.com>
2019-10-10 20:09:27 +0200

    Create index, create and update action
    For votings controller

--- app/controllers/v1/votings_controller.rb
+++ app/controllers/v1/votings_controller.rb
@@ -1,6 +1,37 @@
 module Api
   module V1
     class VotingsController < ApplicationController
+      def index
+        votings = Voting.all
+        render json: {status: 'SUCCESS', message: 'Loaded voteings', data: votings}, status: :ok
+      end
+
+      def create
+        voting = Voting.new(voting_params)
+
+        if voting.save
+          render json: {status: 'SUCCESS', message: 'Saved voting', data: voting}, status: :ok
+        else
+          render json: {status: 'ERROR', message: 'Voting not saved',
+            data: voting.errors}, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        voting = Voting.find(params[:id])
+        if voting.update_attributes(voting_params)
+          render json: {status: 'SUCCESS', message: 'Updated voting', data: voting}, status: :ok
+        else
+          render json: {status: 'ERROR', message: 'Voting not updated',
+            data: voting.errors}, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def voting_params
+        params.permit(:name, :description, :number_of_votes)
+      end
     end
   end
 end
