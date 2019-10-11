5e3acc9162b720281629ab4b8677a9f48d414070
Magdalena Kotołowska <m.kotolowska@gmail.com>
2019-10-11 09:10:04 +0200

    Create index, show and create methods
    For votes controller

--- app/controllers/v1/votes_controller.rb
+++ app/controllers/v1/votes_controller.rb
@@ -1,6 +1,34 @@
 module Api
   module V1
     class VotesController < ApplicationController
+      def index
+        voting = Voting.find(params[:voting_id])
+        votes = voting.votes
+
+        render json: {status: 'SUCCESS', message: 'Loaded votes', data: votes}, status: :ok
+      end
+
+      def show
+        votes = Vote.find(params[:id])
+        render json: {status: 'SUCCESS', message: 'Show vote', data: votes}, status: :ok
+      end
+
+      def create
+        vote = Vote.new(vote_params)
+
+        if vote.save
+          render json: {status: 'SUCCESS', message: 'Saved vote', data: vote}, status: :ok
+        else
+          render json: {status: 'ERROR', message: 'Vote not saved',
+            data: vote.errors}, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def vote_params
+        params.permit(:name, :number, :voting_id)
+      end
     end
   end
 end
